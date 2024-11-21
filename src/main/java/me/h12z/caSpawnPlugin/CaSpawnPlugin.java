bb8132d3078593ede517ab8616ef5a1b4985f510
Henrik Zänger <ZanHe644@MLG.EDUC.LOCAL>
2024-11-21 14:57:00 +0100

    init

---
+++ src/main/java/me/h12z/caSpawnPlugin/CaSpawnPlugin.java
@@ -0,0 +1,13 @@
+package me.h12z.caSpawnPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CaSpawnPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+}
